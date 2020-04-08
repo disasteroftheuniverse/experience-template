8f19e212e49813b72163ac9b4033e5c511b8666d
Patrick Pineda <patrick@patrickpineda.net>
2020-04-08 16:55:44 -0500

    added presets

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ var ajax = require('can-ajax');
 require('aframe');
 require('aframe-extras');
 
+//require('./maya/exports/start.bin');
+//require('./maya/exports/start.gltf');
+
 
 
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,28 @@
 /*jshint esversion: 8*/
 const path = require('path');
+const slash = require('slash');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {
    CleanWebpackPlugin
 } = require('clean-webpack-plugin');
+
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
 var styleParser = require('aframe/src/utils/styleParser');
 var objectPath = require('object-path');
 var _ = require('lodash');
 
 var XMLParser = require('xml2json');
 var fs = require('fs');
+const fsExtra = require('fs-extra');
+
+var htmlPresets = require('./presets');
+
+const protocolify = require('protocolify');
+const gltfPipeline = require('gltf-pipeline');
+const gltfToGlb = gltfPipeline.gltfToGlb;
+const processGltf = gltfPipeline.processGltf;
 
 var attrPreset = function (obj, keypath) {
    var keys = keypath.split('.');
@@ -23,24 +36,36 @@ var attrPreset = function (obj, keypath) {
    return attr;
 };
 
-var rawConfig = JSON.parse(XMLParser.toJson(fs.readFileSync(path.resolve(__dirname, './../config.xml'), {
+var rawConfig = JSON.parse(XMLParser.toJson(fs.readFileSync(path.resolve(__dirname, './../scene.config.xml'), {
    encoding: 'utf-8'
 })));
 var sceneConfig = rawConfig.presets;
-//var renderSettings = attrPreset(sceneConfig,'scene.renderer');
 
-//console.dir(renderSettings);
-module.exports = {
-   mode: 'development',
-   entry: {
-      index: path.join(__dirname, './../', 'src', 'index')
+var htmlTemplateParameters =  {
+   $browseHappy: htmlPresets.browserHappy,
+   $preset: function (prop) {
+      return attrPreset(sceneConfig, prop);
    },
-   output: {
+   $asset: function (id, asseturl) {
+      return `<a-asset-item id="${id}" src="${asseturl}"></a-asset-item>`;
+   }
+};
+
+
+var WebpackConfigX = function (mode) {
+   //mode: 'development',
+   this.mode = mode;
+
+   this.entry = {
+      index: path.resolve(__dirname, './../', 'src', 'index')
+   };
+
+   this.output = {
       path: path.resolve(__dirname, './../dist'),
       filename: '[name].js',
-      //chunkFilename: '[name].js'
-   },
-   module: {
+   };
+
+   this.module = {
       rules: [{
             test: /.jsx?$/,
             include: [
@@ -65,62 +90,145 @@ module.exports = {
             loader: 'ejs-loader'
          },
          {
-            test: /\.txt$/,
+            test: /\.(gltf|bin|glb|obj|fbx)$/,
             loader: 'file-loader',
             options: {
-               outputPath: (url,resourcePath, context)=>{
-                  var boops = path.parse(resourcePath);
-                  //console.log(boops);
-                  var subdir='';
-
-                  switch (boops.ext) {
-                     case '.txt':
-                        subdir='text/';
-                        break;
-                     case '.png':
-                        subdir='images/';
-                        break;
-                     default:
-                        subdir='';
-                  }
-
-
-                  return `/assets/${subdir}${url}`;
-               },
                name: '[name].[ext]',
-               esModule: false
-            }
-         }
-      ]
-   },
-   resolve: {
-      extensions: ['.json', '.js', '.jsx']
-   },
-   plugins: [
-      new HTMLWebpackPlugin({
-         inject: 'head',
-         title: 'My VR Content',
-         template: 'src/index.html',
-         templateParameters: {
-            //renderSettings
-            $preset: function (prop) {
-               return attrPreset(sceneConfig, prop);
-            },
-            $asset: function (id, asseturl) {
-               //require(`file-loader!${asseturl}`);
-               return `<a-asset-item id="${id}" src="${asseturl}"></a-asset-item>`;
+               esModule: false,
+               publicPath: (url, resourcePath, context) => {
+                  if (mode == 'development') {
+                     var relPath = './' + path.posix.join('./', slash(path.relative(path.resolve(__dirname, './../'), path.parse(resourcePath).dir)), url);
+                     return relPath;
+                  } else {
+                     return `./assets/models/${url}`;
+                  }
+               },
+               outputPath: (url, resourcePath, context) => {
+                  return `./assets/models/${url}`;
+               }
             }
          },
+      ]
+   };
+
+   this.resolve = {
+      extensions: ['.json', '.js', '.jsx']
+   };
+
+   this.plugins = [
+      new HTMLWebpackPlugin({
+         title: 'My VR Template',
+         template: 'src/index.html',
+         inject: 'head',
+         templateParameters: htmlTemplateParameters,
          filename: 'index.html',
          chunks: ['index']
       }),
       new CleanWebpackPlugin()
-   ],
-   devtool: 'source-map',
-   devServer: {
-      contentBase: path.join(__dirname, './../', 'dist'),
-      inline: true,
-      host: 'localhost',
-      port: 8080,
+   ];
+
+   if (mode == 'development') {
+      this.devtool = 'source-map';
+      this.devServer = {
+         bonjour: true,
+         contentBase: path.resolve(__dirname, './../'),
+         inline: true,
+         host: 'localhost',
+         port: 8080,
+      };
    }
+
+   if (mode === 'production') {
+      this.optimization = {
+         minimizer: [
+            new TerserPlugin({
+               extractComments: 'all',
+               //exclude: /[\\/]angular[\\/]/,
+               // parallel: true,
+               terserOptions: {
+                  drop_console: false,
+                  ecma: undefined,
+                  warnings: false,
+                  parse: {},
+                  compress: {},
+                  mangle: true, // Note `mangle.properties` is `false` by default.
+                  module: true,
+                  output: null,
+                  toplevel: false,
+                  nameCache: null,
+                  ie8: false,
+                  keep_classnames: undefined,
+                  keep_fnames: false,
+                  safari10: false,
+               },
+               test: /\.js(\?.*)?$/i,
+            }),
+         ],
+      };
+      this.plugins.push(new CompressionPlugin({
+         filename: '[path].gz[query]',
+         algorithm: 'gzip',
+         test: /\.(js|html|css|glb|gltf|bin|obj|fbx)$/,
+         compressionOptions: {
+            level: 9
+         },
+         //threshold: 0,
+         minRatio: Number.MAX_SAFE_INTEGER,
+         deleteOriginalAssets: true,
+      }));
+   }
+
+   return this;
+
 };
+
+module.exports = (env, argv) => {
+   return new WebpackConfigX(argv.mode);
+};
+
+/*      new CompressionPlugin({
+         filename: '[path].gz[query]',
+         algorithm: 'gzip',
+         test: /\.(js|css|svg)$/,
+         compressionOptions: { level: 9 },
+         //threshold: 0,
+         minRatio: 1,
+         deleteOriginalAssets: true,
+       }),
+      new webpack.ProvidePlugin({
+         $: 'jquery',
+         jQuery: 'jquery'
+      }),
+      new HardSourceWebpackPlugin({
+         cacheDirectory: 'C://tmp//supervr//cache'
+      }),
+   ],
+   resolve: {
+      extensions: ['.json', '.js', '.jsx']
+   },
+   optimization: {
+      minimizer: [
+           new TerserPlugin({
+            extractComments: 'all',
+            //exclude: /[\\/]angular[\\/]/,
+           // parallel: true,
+            terserOptions: {
+              drop_console: false,
+              ecma: undefined,
+              warnings: false,
+              parse: {},
+              compress: {},
+              mangle: true, // Note `mangle.properties` is `false` by default.
+              module: true,
+              output: null,
+              toplevel: false,
+              nameCache: null,
+              ie8: false,
+              keep_classnames: undefined,
+              keep_fnames: false,
+              safari10: false,
+            },
+            test: /\.js(\?.*)?$/i,
+          }),
+        ],
+    }*/
